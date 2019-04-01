56990ca067cde92df1910219b1639c8ae7b14587
Tung Tran <tungtqse02084@gmail.com>
2019-04-01 14:39:33 +0800

    Create Project

--- WebApi/Models/IdentityModels.cs
+++ WebApi/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace WebApi.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("WAConnection", throwIfV1Schema: false)
         {
         }
         
